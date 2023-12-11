64df401e577e8deac4a772d423935b26b392a707
pambos777 <chrpamboss@gmail.com>
2023-12-11 20:40:25 +0200

    portfoliooo

---
+++ src/components/Interface.jsx
@@ -0,0 +1,353 @@
+import { ValidationError, useForm } from "@formspree/react";
+import { motion } from "framer-motion";
+import { useAtom } from "jotai";
+import { currentProjectAtom, projects } from "./Projects";
+
+const Section = (props) => {
+  const { children, mobileTop } = props;
+
+  return (
+    <motion.section
+      className={`
+  h-screen w-screen p-1.5 max-w-screen-2xl mx-auto
+  flex flex-col items-start
+  ${mobileTop ? "justify-start md:justify-center" : "justify-center"}
+  `}
+      initial={{
+        opacity: 0,
+        y: 50,
+      }}
+      whileInView={{
+        opacity: 1,
+        y: 0,
+        transition: {
+          duration: 1,
+          delay: 0.6,
+        },
+      }}
+    >
+      {children}
+    </motion.section>
+  );
+};
+
+export const Interface = (props) => {
+  const { setSection } = props;
+  return (
+    <div className="flex flex-col items-center w-screen">
+      <AboutSection setSection={setSection} />
+      <SkillsSection />
+      <ProjectsSection />
+      <ContactSection />
+    </div>
+  );
+};
+
+const AboutSection = (props) => {
+  const { setSection } = props;
+  return (
+    <Section mobileTop>
+      <h1 className="text-5xl font-extrabold leading-snug">
+        Hello, I'm
+        <br />
+        <span className="bg-white px-1 italic">Charalambos Poyiadjis</span>
+      </h1>
+      <motion.p
+        className="font-bold text-lg text-white mt-4"
+        initial={{
+          opacity: 0,
+          y: 25,
+        }}
+        whileInView={{
+          opacity: 0.87,
+          y: 0,
+        }}
+        transition={{
+          duration: 1,
+          delay: 1.5,
+        }}
+      >
+        A strongly motivated individual that has a passion on all aspects of technology,  
+        <br />
+        always striving to learn and do more. Recently achieved CCNA Certification with Merrit   
+        <br />
+        in all 3 modules and has more than 2 years of working experiece in IT 
+        <br />
+        after graduating from Computer Science degree.
+      </motion.p>
+      <a  href="/projects/CharalambosPoyiadjisCV2023.docx" download>
+        <motion.button 
+        className={`bg-indigo-600 text-white py-4 px-8 
+        rounded-lg font-bold text-lg mt-5`}
+        initial={{
+          opacity: 0,
+          y: 25,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 1,
+          delay: 1.7,
+        }}
+      >
+      Download my CV
+     </motion.button>
+      </a>
+      <motion.button 
+        onClick={() => setSection(3)}
+        className={`bg-indigo-600 text-white py-4 px-8 
+      rounded-lg font-bold text-lg mt-5`} 
+        initial={{
+          opacity: 0,
+          y: 25,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 1,
+          delay: 2,
+        }}
+      >
+      Contact
+     </motion.button>
+
+    </Section>
+  );
+};
+
+const skills = [
+  {
+    title: "Cisco IOS",
+    level: 95,
+  },
+  {
+    title: "Cisco Packet Tracer",
+    level: 90,
+  },
+  {
+    title: "HTML, CSS, PHP, JavaScript, React.js ",
+    level: 90,
+  },
+  {
+    title: "MYSQL, SQLite Access",
+    level: 70,
+  },
+  {
+    title: "Object Oriented Java, Event Driven C#",
+    level: 60,
+  },
+  {
+    title: "jQUERY,Three.js,Nodejs",
+    level: 70,
+  },
+];
+const languages = [
+  {
+    title: "🇬🇧 English",
+    level: 100,
+  },
+  {
+    title: "🇬🇷 Greek",
+    level: 100,
+  },
+];
+
+const SkillsSection = () => {
+  return (
+    <Section>
+      <motion.div className="w-full" whileInView={"visible"}>
+        <h2 className="text-3xl md:text-5xl font-bold text-white">Skills</h2>
+        <div className="mt-8 space-y-4">
+          {skills.map((skill, index) => (
+            <div className="w-full md:w-64" key={index}>
+              <motion.h3
+                className="text-lg md:text-xl font-bold text-gray-100"
+                initial={{
+                  opacity: 0,
+                }}
+                variants={{
+                  visible: {
+                    opacity: 1,
+                    transition: {
+                      duration: 1,
+                      delay: 1 + index * 0.2,
+                    },
+                  },
+                }}
+              >
+                {skill.title}
+              </motion.h3>
+              <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
+                <motion.div
+                  className="h-full bg-indigo-500 rounded-full "
+                  style={{ width: `${skill.level}%` }}
+                  initial={{
+                    scaleX: 0,
+                    originX: 0,
+                  }}
+                  variants={{
+                    visible: {
+                      scaleX: 1,
+                      transition: {
+                        duration: 1,
+                        delay: 1 + index * 0.2,
+                      },
+                    },
+                  }}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+        <div>
+          <h2 className="text-3xl md:text-5xl font-bold mt-10 text-white">
+            Languages
+          </h2>
+          <div className="mt-8 space-y-4">
+            {languages.map((lng, index) => (
+              <div className="w-full md:w-64" key={index}>
+                <motion.h3
+                  className="text-lg md:text-xl font-bold text-gray-100"
+                  initial={{
+                    opacity: 0,
+                  }}
+                  variants={{
+                    visible: {
+                      opacity: 1,
+                      transition: {
+                        duration: 1,
+                        delay: 2 + index * 0.2,
+                      },
+                    },
+                  }}
+                >
+                  {lng.title}
+                </motion.h3>
+                <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
+                  <motion.div
+                    className="h-full bg-indigo-500 rounded-full "
+                    style={{ width: `${lng.level}%` }}
+                    initial={{
+                      scaleX: 0,
+                      originX: 0,
+                    }}
+                    variants={{
+                      visible: {
+                        scaleX: 1,
+                        transition: {
+                          duration: 1,
+                          delay: 2 + index * 0.2,
+                        },
+                      },
+                    }}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </motion.div>
+    </Section>
+  );
+};
+
+const ProjectsSection = () => {
+  const [currentProject, setCurrentProject] = useAtom(currentProjectAtom);
+
+  const nextProject = () => {
+    setCurrentProject((currentProject + 1) % projects.length);
+  };
+
+  const previousProject = () => {
+    setCurrentProject((currentProject - 1 + projects.length) % projects.length);
+  };
+
+  return (
+    <Section>
+      <div className="flex w-full h-full gap-8 items-center justify-center">
+        <button
+          className="hover:text-indigo-600 transition-colors"
+          onClick={previousProject}
+        >
+          ← Previous
+        </button>
+        <h2 className="text-3xl md:text-5xl font-bold">Certifications</h2>
+        <button
+          className="hover:text-indigo-600 transition-colors"
+          onClick={nextProject}
+        >
+          Next →
+        </button>
+      </div>
+    </Section>
+  );
+};
+
+const ContactSection = () => {
+  const [state, handleSubmit] = useForm("xqkvvloo");
+  return (
+    <Section>
+      <h2 className="text-3xl md:text-5xl font-bold">Contact me</h2>
+      <div className="mt-8 p-8 rounded-md bg-white bg-opacity-50 w-96 max-w-full">
+        {state.succeeded ? (
+          <p className="text-gray-900 text-center">Thanks for your message !</p>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <label for="name" className="font-medium text-gray-900 block mb-1">
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+            />
+            <label
+              for="email"
+              className="font-medium text-gray-900 block mb-1 mt-8"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+            />
+            <ValidationError
+              className="mt-1 text-red-500"
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+            <label
+              for="email"
+              className="font-medium text-gray-900 block mb-1 mt-8"
+            >
+              Message
+            </label>
+            <textarea
+              name="message"
+              id="message"
+              className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 p-3"
+            />
+            <ValidationError
+              className="mt-1 text-red-500"
+              errors={state.errors}
+            />
+            <button
+              disabled={state.submitting}
+              className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 "
+            >
+              Submit
+            </button>
+          </form>
+        )}
+      </div>
+    </Section>
+  );
+};
