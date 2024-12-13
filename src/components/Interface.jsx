b74a50b66b6b0553220661b884c36b6343abe8fe
pambos777 <153544956+pambos777@users.noreply.github.com>
2024-12-13 02:08:40 +0200

    Update Interface.jsx

--- src/components/Interface.jsx
+++ src/components/Interface.jsx
@@ -75,7 +75,7 @@ const AboutSection = (props) => {
         <br />
         after graduating from Computer Science degree.
       </motion.p>
-      <a  href="/projects/Charalambos-PoyiadjisCV.docx" download>
+      <a  href="/projects/CharalambosPoyiadjisCV2023.docx" download>
         <motion.button 
         className={`bg-indigo-600 text-white py-4 px-8 
         rounded-lg font-bold text-lg mt-5`}
